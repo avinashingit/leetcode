ef4f996ee43926d6ed91d36da146b697ddcc65cd
Avinash Kadimisetty <kavinash366@gmail.com>
2019-09-01 21:36:07 -0500

    twosum

---
+++ explore/amazon/two_sum.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = defaultdict(int)
+        for i, num in enumerate(nums):
+            if (target - num) in indices:
+                return [i, indices[target-num]]
+            else:
+                indices[num] = i
+        return []
