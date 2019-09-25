1317e8a808166c8a1537aa3ea4e1897b88553902
Avinash Kadimisetty <kavinash366@gmail.com>
2019-09-25 18:43:54 -0500

    post order traversal iterative

---
+++ explore/amazon/binary_tree_postorder_traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        stack = []
+        stack.append(root)
+        result = []
+        while stack:
+            node = stack.pop()
+            if not node:
+                continue
+            result.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return result[::-1]
+            
